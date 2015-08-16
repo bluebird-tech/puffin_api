bca9653255688df1196b8b179a00c3b4f1a94b15
Frank C. Eckert <frank.eckert@boost-project.com>
2015-08-16 12:37:33 +0200

    include go-json-rest and postgres

--- api.go
+++ api.go
@@ -1,13 +1,71 @@
 package main
 
 import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
-	http.Handle("/", helloHandler())
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	i := Impl{}
+	i.InitDB()
+	i.InitSchema()
+
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Post("/events", i.PostEvent),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), api.MakeHandler()))
+}
+
+type Event struct {
+	Id          int64     `json:"id"`
+	Measurement string    `sql:"size:1024" json:"measurement"`
+	Fields      string    `sql:"size:1024" json:"fields"`
+	Tags        string    `sql:"size:1024" json:"tags"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	DeletedAt   time.Time `json:"-"`
+}
+
+type Impl struct {
+	DB gorm.DB
+}
+
+func (i *Impl) InitDB() {
+	var err error
+	connection := os.Getenv("DATABASE_URL")
+	i.DB, err = gorm.Open("postgres", connection)
+	if err != nil {
+		log.Fatalf("Got error when connect database, the error is '%v'", err)
+	}
+	i.DB.LogMode(true)
+}
+
+func (i *Impl) InitSchema() {
+	i.DB.AutoMigrate(&Event{})
+}
+
+func (i *Impl) PostEvent(w rest.ResponseWriter, r *rest.Request) {
+	event := Event{}
+	if err := r.DecodeJsonPayload(&event); err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := i.DB.Save(&event).Error; err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteJson(&event)
 }
 
 func helloHandler() http.Handler {
