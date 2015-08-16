041626cff6c4a6b117a70b792cd3f2e0374d79aa
Frank C. Eckert <frank.eckert@boost-project.com>
2015-08-16 13:07:18 +0200

    add newrelic support

--- api.go
+++ api.go
@@ -4,6 +4,8 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"github.com/yvasiyarov/go-metrics"
+	"github.com/yvasiyarov/gorelic"
 	"log"
 	"net/http"
 	"os"
@@ -17,6 +19,11 @@ func main() {
 
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
+	api.Use(&NewRelicMiddleware{
+		License: os.Getenv("NEWRELIC_LICENCE_KEY"),
+		Name:    "Puffin Event API",
+		Verbose: true,
+	})
 	router, err := rest.MakeRouter(
 		rest.Post("/events", i.PostEvent),
 	)
@@ -25,6 +32,33 @@ func main() {
 	}
 	api.SetApp(router)
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), api.MakeHandler()))
+}
+
+type NewRelicMiddleware struct {
+	License string
+	Name    string
+	Verbose bool
+	agent   *gorelic.Agent
+}
+
+func (mw *NewRelicMiddleware) MiddlewareFunc(handler rest.HandlerFunc) rest.HandlerFunc {
+
+	mw.agent = gorelic.NewAgent()
+	mw.agent.NewrelicLicense = mw.License
+	mw.agent.HTTPTimer = metrics.NewTimer()
+	mw.agent.Verbose = mw.Verbose
+	mw.agent.NewrelicName = mw.Name
+	mw.agent.CollectHTTPStat = true
+	mw.agent.Run()
+
+	return func(writer rest.ResponseWriter, request *rest.Request) {
+
+		handler(writer, request)
+
+		// the timer middleware keeps track of the time
+		startTime := request.Env["START_TIME"].(*time.Time)
+		mw.agent.HTTPTimer.UpdateSince(*startTime)
+	}
 }
 
 type Event struct {
