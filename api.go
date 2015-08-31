01d7aec48f7fd89f2938ef18bf6e9cba7a7f6ee3
Frank C. Eckert <frank.eckert@boost-project.com>
2015-08-31 15:54:45 +0200

    add float value field to api

--- api.go
+++ api.go
@@ -68,6 +68,7 @@ type Event struct {
 	DeviceId    string    `sql:"size:40" json:"deviceId"`
 	MeasuredAt  time.Time `json:"measuredAt"`
 	Measurement string    `sql:"size:1024" json:"measurement"`
+	Value       float64   `json:"value"`
 	Fields      string    `sql:"size:1024" json:"fields"`
 	Tags        string    `sql:"size:1024" json:"tags"`
 	CreatedAt   time.Time `json:"createdAt"`
